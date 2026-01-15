2033f02cf0dcbf44d3b7bda37d7628115aaa02a3
Martynasstakvilionis <stakynas@gmail.com>
2026-01-15 13:02:18 +0100

    fredag
    er det fredag?

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-import flask
+import os, sys, json, datetime, re, random, string, shutil, threading
 from flask import Flask, jsonify, request, abort, make_response, url_for, redirect, render_template, flash
 
+
+
+fredag = datetime.datetime.now().weekday() == 4
+if fredag:
+    if_fredag = "Ja"
+else:
+    if_fredag = "Nei"
+    
 
 
 app = Flask(__name__)
@@ -9,7 +17,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', if_fredag=if_fredag)
 
 if __name__ == '__main__':
     app.run(debug=True)
