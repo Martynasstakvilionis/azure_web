785e9622a5ffe72e6a213b0f67029840b21725fc
Martynasstakvilionis <stakynas@gmail.com>
2026-01-30 13:55:00 +0100

    fiksa potensiel en oppdaterings problem
    nå funker det faktis. trur jeg

--- app.py
+++ app.py
@@ -2,22 +2,19 @@ import os, sys, json, datetime, re, random, string, shutil, threading
 from flask import Flask, jsonify, request, abort, make_response, url_for, redirect, render_template, flash
 
 
-
-fredag = datetime.datetime.now().weekday() == 4
-if fredag:
-    if_fredag = "Ja"
-else:
-    if_fredag = "Nei"
-    
-
-
 app = Flask(__name__)
-
 
 
 @app.route('/')
 def index():
+    fredag = datetime.datetime.now().weekday() == 4
+    if fredag: 
+        if_fredag = "Ja"
+    else: 
+        if_fredag = "Nei"
     return render_template('index.html', if_fredag=if_fredag)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
